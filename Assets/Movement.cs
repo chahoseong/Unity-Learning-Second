6f9166f7fefca077b71a48faf0138f1a9d99750b
Cha Hoseong <hoseong@chahoseong.com>
2022-11-26 20:13:18 +0900

    init project

---
+++ Assets/Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float boost;
+    [SerializeField] private float angularSpeed;
+
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Rotate();
+        Move();
+    }
+
+    private void Rotate()
+    {
+        // 왼쪽으로 회전
+        Vector3 rotation = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            // z축으로 x만큼 회전
+            rotation.z += angularSpeed;
+        }
+        // 오른쪽으로 회전
+        if (Input.GetKey(KeyCode.D))
+        {
+            // z축으로 -x만큼 회전
+            rotation.z -= angularSpeed;
+        }
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.freezeRotation = true;
+            Vector3 force = Vector3.up * (boost * Time.deltaTime);
+            rigidbody.AddRelativeForce(force);
+            rigidbody.freezeRotation = false;
+        }
+    }
+}
